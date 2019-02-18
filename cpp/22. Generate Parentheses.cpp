4b18dd1825bc61b113e91a54845acd2354cb8daf
Chenning Li <18801066306@163.com>
2019-02-18 19:41:48 +0800

    Create 22. Generate Parentheses.cpp

---
+++ cpp/22. Generate Parentheses.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<string> generateParenthesis(int n) {
+        vector<string> res;
+        addingpar(res, "", n, 0);
+        return res;
+    }
+    void addingpar(vector<string> &v, string str, int n, int m){
+        if(n==0 && m==0) {
+            //推入结果
+            v.push_back(str);
+            return;
+        }
+        //递归就是穷举分叉
+        if(m > 0){ addingpar(v, str+")", n, m-1); }
+        if(n > 0){ addingpar(v, str+"(", n-1, m+1); }
+    }
+};
