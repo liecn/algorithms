b44ca10f5dfe5520e7d36e8b42553bdf0c99edb7
Chenning Li <18801066306@163.com>
2019-04-04 16:57:19 +0800

    Create 5.LongestPalindromicSubstring.py

---
+++ python/5.LongestPalindromicSubstring.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def longestPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        if s is '': 
+            return s
+        max_len = 0 
+        start, end = 0, 0
+        for i in range(len(s)):
+            len1 = self.expandFromCenter(s, i, i)
+            len2 = self.expandFromCenter(s, i, i+1)
+            l = max(len1, len2)
+            if l > end - start:
+                start = i - (l - 1) // 2
+                end = i + l // 2
+
+        return s[start:end+1]
+
+    def expandFromCenter(self, s, idx1, idx2):
+        while idx1 >= 0 and idx2 < len(s) and s[idx1] == s[idx2]:
+            idx1 -= 1
+            idx2 += 1
+        return idx2 - idx1 - 1 
