248fd97dce89604d533574ca82a5bbcbd812d45a
Chenning Li <18801066306@163.com>
2019-04-17 23:08:02 +0800

    Create 279.PerfectSquares.java

---
+++ java/279.PerfectSquares.java
@@ -0,0 +1,15 @@
+class Solution {
+   public int numSquares(int n) {
+	int[] dp = new int[n + 1];
+    Arrays.fill(dp, Integer.MAX_VALUE);
+	dp[0] = 0;
+	for(int i = 1; i <= n; ++i) {
+		int j = 1;
+		while(i - j*j >= 0) {
+			dp[i]  = Math.min(dp[i] , dp[i - j*j] + 1);
+			++j;
+		}
+	}		
+	return dp[n];
+    }
+}
